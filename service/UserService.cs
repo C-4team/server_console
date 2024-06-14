e8f9e322b47d36f537bd43f34e411c27e641c29f
seyeon22 <sywolleych@gmail.com>
2024-06-14 21:49:47 +0900

    user service를 수정

--- service/UserService.cs
+++ service/UserService.cs
@@ -10,23 +10,23 @@ namespace Service.userService{
         }
 
         // 회원가입
-        public bool Register(User newUser) {
+        public int Register(User newUser) {
             var existUser = userRepository.Get(newUser.Id);
             if (existUser != null && existUser.Valid == 1)
-                return false;  // 기존 유저는 회원가입 X
+                return 0;  // 기존 유저는 회원가입 X
             newUser.Valid = 1;   // 신규유저 -> 기존유저
             userRepository.Insert(newUser);
-            return true;
+            return 1;
         }
 
         // 로그인
-        public bool Login(long id, string password)
+        public int Login(long id, string password)
         {
             var existUser = userRepository.Get(id);
             if (existUser != null && existUser.Valid == 1
                 && existUser.Password == password)
-                return true;  // 로그인 성공
-            else return false;  // 로그인 실패
+                return 3;  // 로그인 성공
+            else return 2;  // 로그인 실패
         }
     }
 }
