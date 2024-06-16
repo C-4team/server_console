57dc2634b62c4ccedde0aa8d03abbbaed1db7bd0
강지훈 <clone0922@gmail.com>
2024-06-16 15:17:13 +0900

    finish almost

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using server_console.dataset;
 using Service.chatservice;
 using Service.dataSetService;
+using Service.serverService;
 
 
 class Program{
     static void Main(string[] args){
         Console.WriteLine("Server Start");
         DataBase db = DataSetService.DB;
-        ChatService chatService = new ChatService();
+        ServerService serverService = new ServerService();
+        serverService.acceptRequestDeamon();
     }
 }
